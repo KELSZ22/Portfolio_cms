e869eb677739453c925a28d99fb6b01bd81ea8cd
KELSZ22 <kelgithub@gmail.com>
2024-11-16 21:27:41 +0800

    responsive layout

--- src/Contents/Home.jsx
+++ src/Contents/Home.jsx
@@ -8,7 +8,7 @@ function Home(){
 
                 <label htmlFor="">About Me</label>
                 
-                 <img src="/about.png" alt="about.png" height="220px"/>
+                 <img src="/about.png" alt="about.png" height="300px" width="300px"/>
 
                 <p>I'm Michael Z. Sabino, a recent computer engineering bachelor's degree graduate, has a strong interest in software development and programming.  I have experience as a full-stack developer from my recent OJT, having worked on a variety of projects utilizing HTML,CSS, Javascript, Java,PHP, Laravel, and other. With  dedication to problem-solving and dedication, I'm looking forward to contribute new ideas and creativity to the tech field.</p>
 
@@ -80,7 +80,7 @@ function Home(){
                 <h1>Contacts</h1>
 
                 <div className="contacts-content">
-                    <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d965.2050919574647!2d120.9630736695074!3d14.609306596862266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ca74b8504f5f%3A0x629564a516e4eb37!2s1319%20Abad%20Santos%20St%2C%20Tondo%2C%20Manila%2C%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1731127033583!5m2!1sen!2sph"  width="500" height="500px"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Address"></iframe></p>
+                    <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d965.2050919574647!2d120.9630736695074!3d14.609306596862266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ca74b8504f5f%3A0x629564a516e4eb37!2s1319%20Abad%20Santos%20St%2C%20Tondo%2C%20Manila%2C%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1731127033583!5m2!1sen!2sph"  width="500" height="400px"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Address"></iframe></p>
     
                     <div className="contacts-input">
 
--- src/Template/_header.jsx
+++ src/Template/_header.jsx
@@ -15,7 +15,7 @@ function Header (){
 
            </div>
 
-            <img src="/cover2.png" alt="michael_pogi.png" height="500px"/>
+            <img src="/cover2.png" alt="michael_pogi.png" height="400px"/>
 
 
         </div>
