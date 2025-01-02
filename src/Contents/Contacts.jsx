e45eef57ef06b0e5a847c89f1ed2c71e91d6ea45
KELSZ22 <kelgithub@gmail.com>
2025-01-02 13:42:18 +0800

    vercel

--- src/Contents/Contacts.jsx
+++ src/Contents/Contacts.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Contacts (){
     return (
         <>
-  <div>
+  <div className="cont">
+                        <h1 className="tacts">Contacts</h1>
 
             <div className="contacts-container">
-                <h1>Contacts</h1>
 
                 <div className="contacts-content">
                     <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d965.2050919574647!2d120.9630736695074!3d14.609306596862266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ca74b8504f5f%3A0x629564a516e4eb37!2s1319%20Abad%20Santos%20St%2C%20Tondo%2C%20Manila%2C%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1731127033583!5m2!1sen!2sph"  width="500" height="400px"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Address"></iframe></p>
--- src/Contents/Projects.jsx
+++ src/Contents/Projects.jsx
@@ -1,140 +1,15 @@
-import {Link} from "react-router-dom";
 
-function Projects (){
 
-    return(
-   <div>
+function Project() {
 
-<<<<<<< HEAD
-            <div className="contacts-container">
-                <h1>Projects</h1>
-
-                <div className="contacts-content">
-                    <p><iframe src="https://mi-calculator-gamma.vercel.app/"  width="500" height="400px"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Address"></iframe></p>
+  return (
+    <> 
+    <h1>Project</h1>
+<h2> Working in Progress</h2>    
     
-                    <div className="contacts-input">
-
-                        <label htmlFor=""> MiCalculator Ver. 1.0</label>
-                        <p> This Project MiCalculator is part of my practice</p>
-                          </div>
-                
-                </div>
-
-                </div>
-=======
-
-<div className="projects-container">
->>>>>>> d71352fcfd65c8a3ec2903572f5e8219aa5b1773
-
-<h1> Projects</h1>
-
-<div className="projects-card-wrap">
-
-   <div className="projects-card">
-
-       <img src="/webbased.png" alt="projects.png" height="200px"/>
-
-       <h4>Web-based Attendance Management System using QR Code</h4>
-
-       <h5> HTML,CSS,Javascript </h5>
-
-
-       <Link to="/project">More</Link>
-
-   </div>
-
-     <div className="projects-card">
-
-       <img src="/salesbilling.png" alt="projects.png" height="200px"/>
-
-       <h4>SunGold Sales and Billing Management System</h4>
-
-       <h5> Java </h5>
-       
-       <Link to="/project">More</Link>
-
-
-
-   </div>
-
-     <div className="projects-card">
-
-       <img src="/udm.png" alt="projects.png" height="200px"/>
-
-       <h4>UDM Library Management System</h4>
-
-       <h5> Java </h5>
-
-       <Link to="/project">More</Link>
-
-
-   </div>
-
-     <div className="projects-card">
-
-       <img src="inventory.png" alt="projects.png" height="200px"/>
-
-       <h4>Inventory Management System</h4>
-
-       <h5> Java </h5>
-       
-       <Link to="/project">More</Link>
-
-
-   </div>
-
-   <div className="projects-card">
-
-<img src="/batteryApp.png" alt="projects.png" height="200px"/>
-
-<h4>Battery App: Battery Life Saver</h4>
-
-<h5> Cordova </h5>
-
-<Link to="/project">More</Link>
-
-
-</div>
-
-<div className="projects-card">
-
-<img src="/myportfolio1.png" alt="projects.png" height="200px"/>
-
-<h4>My Portfolio</h4>
-
-<h5> HTML, CSS, Javascript </h5>
-<a href="https://kelsz22.github.io/My_Portfolio/?fbclid=IwY2xjawHGcV1leHRuA2FlbQIxMAABHaBSP2ilfuXb0sgglEx-Tjg2UbEMY21xq8KxdCJHlxOgDtfzVDfURhwW8g_aem__8FO_PwoYeT0DWarNCquFA" target="_blank"> MyPortfolio</a>
-<Link to="/project">More</Link>
-
-
-</div>
-
-
-<div className="projects-card">
-
-<img src="/wordpress.png" alt="projects.png" height="200px"/>
-
-<h4>My Portfolio</h4>
-
-<h5> Wordpress </h5>
-<a href="https://nidali0910.wordpress.com/" target="_blank"> MyPortfolio</a>
-<Link to="/project">More</Link>
-
-
-</div>
-
-
-</div>
-
-
-
-
-</div>
-
-
-    );
-
-
+     </>
+  );
 
 }
-export default Projects;
+
+export default Project;
